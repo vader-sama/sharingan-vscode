c920b2eb674dcbb8b481ac607eaab31368601fdd
CEO of Death Star <seanthevn@gmail.com>
2025-01-22 03:23:59 +0330

    feat: rotate sharingan on type

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
+  const icons = [
+    "$(sharingan-0-deg)",
+    "$(sharingan-45-deg)",
+    "$(sharingan-90-deg)",
+    "$(sharingan-135-deg)",
+    "$(sharingan-180-deg)",
+    "$(sharingan-225-deg)",
+    "$(sharingan-270-deg)",
+    "$(sharingan-315-deg)",
+  ];
+  let index = 0;
+  let isVisible = true;
+  const statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right
+  );
+  statusBarItem.text = `${icons[index]}`;
+  statusBarItem.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sharingan" is now active!');
+  const onTextChanged = vscode.workspace.onDidChangeTextDocument((event) => {
+    if (
+      vscode.window.activeTextEditor &&
+      event.document === vscode.window.activeTextEditor.document
+    ) {
+      if (!icons[index]) {
+        index = 0;
+      }
+      statusBarItem.text = `${icons[index]}`;
+      index++;
+    }
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sharingan.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sharingan!');
-	});
-
-	context.subscriptions.push(disposable);
+  const toggleStatusBarCommand = vscode.commands.registerCommand(
+    "extension.toggleSharingan",
+    () => {
+      isVisible = !isVisible;
+      if (isVisible) {
+        statusBarItem.show();
+      } else {
+        statusBarItem.hide();
+      }
+    }
+  );
+  context.subscriptions.push(onTextChanged, toggleStatusBarCommand);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
